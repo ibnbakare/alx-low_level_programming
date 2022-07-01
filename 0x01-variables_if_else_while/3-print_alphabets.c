0eea74431cb69711c10ef7c5ad339b4b11fa5ed0
ibnbakare <ridmartrite@gmail.com>
2022-07-01 20:53:36 +0100

    3-print alpha

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main -> assign a random number to the variable n each time it is executed
+ *and print the last digit of the number stored in the variable n
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+int ch;
+for (ch=a; ch<=z; ch++)
+{
+    putchar(ch);
+}
+
+putchar(10);
+return (0);
+}
