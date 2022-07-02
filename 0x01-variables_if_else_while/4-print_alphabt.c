6bcad4949679cad740f69ed68c7bafe72f85c9e5
ibnbakare <ridmartrite@gmail.com>
2022-07-02 04:39:22 +0100

    printing base10 with putchar

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 /**
  *main -> assign a random number to the variable n each time it is executed
- *and print the last digit of the number stored in the variable n
+ *and print the last digit of the number stored in the variable
  *Return: Always 0 (Success)
  */
 int main(void)
