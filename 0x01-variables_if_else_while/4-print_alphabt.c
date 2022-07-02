aeb05ab660877705c58d73291a29eecb6078909e
ibnbakare <ridmartrite@gmail.com>
2022-07-02 03:43:12 +0100

    adding 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * this should print of the values of ch
- * in lowercacses
+ *main -> assign a random number to the variable n each time it is executed
+ *and print the last digit of the number stored in the variable n
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
 int ch;
-for (ch = "97"; ch <= "122"; ch++)
+for (ch = 97; ch <= 122; ch++)
 {
-if(ch != "113" && ch != "101")
+if (ch != 113 && ch != 101)
 putchar(ch);
 putchar(10);
 }
