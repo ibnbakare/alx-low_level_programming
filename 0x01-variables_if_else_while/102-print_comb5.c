ffab0d33187bac4d035978a850c0187b23f25eee
ibnbakare <ridmartrite@gmail.com>
2022-07-02 05:48:24 +0100

    adding

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  *main -> assign a random number to the variable n each time it is executed
  *and print the last digit of the number stored in the variable n
@@ -7,6 +8,7 @@
 int main(void)
 {
 int i, j;
+
 for (i = 0; i < 100; i++)
 {
 for (j = 0; j < 100; j++)
@@ -26,6 +28,6 @@ putchar(' ');
 }
 }
 }
-}
 putchar('\n');
 return (0);
+}
