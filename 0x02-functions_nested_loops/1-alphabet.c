150708164d74c8bdcccea40662571b20b643a703
ibnbakare <ridmartrite@gmail.com>
2022-07-05 15:22:38 +0100

    adding task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *main -> assign a random number to the variable n each time it is executed
+ *and print the last digit of the number stored in the variable n
+ *Return: Always 0 (Success)
+ */
+
+void print_alphabet(void);
+{
+int ch;
+for(ch = 97; ch <= 122; ch++)
+{
+    _putchar(ch);
+    _putchar(20);
+}
+}
