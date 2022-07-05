321001e356208fb9bee9ffb7c2938d76007dca18
ridhwan <ridmartrite@gmail.com>
2022-07-05 05:19:42 -0700

    adding .h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <main.h>
 /**
  *main -> assign a random number to the variable n each time it is executed
  *and print the last digit of the number stored in the variable n
