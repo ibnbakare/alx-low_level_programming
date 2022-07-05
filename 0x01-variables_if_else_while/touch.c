18ff2672d15b69e0fdc77ec8ae0056ca86c6741b
ridhwan <ridmartrite@gmail.com>
2022-07-05 05:06:04 -0700

    sds

---
+++ 0x01-variables_if_else_while/touch.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/** main- computing the size of data types
+ * Return coorect values
+ */
+int main(void)
+{
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+return (0);
+}
+
