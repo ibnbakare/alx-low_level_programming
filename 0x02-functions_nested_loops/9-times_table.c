ab611b7a1bdc0ead7d389650d92212d9b965558c
ridhwan <ridmartrite@gmail.com>
2022-07-05 21:47:04 -0700

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,12 +15,13 @@ k = x * y;
 if (y == 0)
 _putchar(k + '0');
 if (y != 0 && k < 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(k + '0');
-} else if (k >= 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(k + '0');
+}
+else if (k >= 10)
 {
 _putchar(',');
 _putchar(' ');
