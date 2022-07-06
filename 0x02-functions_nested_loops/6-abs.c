db38a61e75238cf588c3deabf19be295bc18d597
ridhwan <ridmartrite@gmail.com>
2022-07-05 23:35:49 -0700

    reset

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs -> return absolute value of a number
+ *@n: parameter
+ *Return: absolute value of a number
+*/
+
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+	return (n);
+	}
+	else
+	{
+	return (n * -1);
+	}
+}
