7dc86ae2fdabc7482a0c45f2cebdcd21f6068b99
ridhwan <ridmartrite@gmail.com>
2022-07-06 03:53:12 -0700

    10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -7,34 +7,7 @@
 * Return: x + y
 */
 
-<<<<<<< HEAD
 int add(int x, int y)
 {
 	return (x + y);
 }
-=======
-	for (a = 0; a < 10; a++)
-	{
-		for (b = 0; b < 10; b++)
-		{
-			c = a * b;
-			if (b == 0)
-				_putchar(c + '0');
-			if (b != 0 && c < 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(c + '0');
-			} else if (c >= 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((c / 10) + '0');
-				_putchar((c % 10) + '0');
-			}
-		}
-		_putchar('\n');
-	}
-}
->>>>>>> 4c1affb799f1eeb0b0626e0569340e6dee46eec2
