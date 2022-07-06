b33e8ac19eb8d9831fd1e7ccf76d9e12dfc22050
ibnbakare <ridmartrite@gmail.com>
2022-07-06 11:49:50 +0100

    10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,33 +1,13 @@
 #include "main.h"
 
 /**
-*times_table -> time table
+* add -> adds two number
+* @x : param1
+* @y : param2
+* Return: x + y
 */
-void times_table(void)
-{
-int a, b, c;
 
-	for (a = 0; a < 10; a++)
-	{
-		for (b = 0; b < 10; b++)
-		{
-			c = a * b;
-			if (b == 0)
-				_putchar(c + '0');
-			if (b != 0 && c < 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(c + '0');
-			} else if (c >= 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((c / 10) + '0');
-				_putchar((c % 10) + '0');
-			}
-		}
-		_putchar('\n');
-	}
+int add(int x, int y)
+{
+	return (x + y);
 }
