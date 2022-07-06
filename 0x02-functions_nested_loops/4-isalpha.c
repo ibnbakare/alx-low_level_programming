8df3304a872ad55a230d6e35134b2d870c3d5384
ridhwan <ridmartrite@gmail.com>
2022-07-05 23:51:53 -0700

    Revert "4" yes
    This reverts commit a58af3f3d9c138137fa29deedf5b2578f2038fa7.

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,9 +4,9 @@
 *@c: a charter to be checke on
 *Return: returns 0 or 1 depending on condition
 */
-int _isalpha(int c)
+int _islower(int c)
 {
-if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+if((c >= 'a' && c <= 'z') || (c >= 'A' && c >= 'Z'))
 return (1);
 else
 return (0);
