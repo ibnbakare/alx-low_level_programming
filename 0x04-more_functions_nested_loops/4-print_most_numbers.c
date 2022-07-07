2d9f314eadc744eae1f45aee0e838c62b0567ada
ibnbakare <ridmartrite@gmail.com>
2022-07-07 18:07:07 +0100

    printing 0 to 9 minus 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* print_most_numbers -> this prints most numbers
+*/
+void print_most_numbers(void)
+{
+int i;
+for (x = 0; i < 10; i++)
+{
+if (i != 2 && i != 4)
+_putchar(i + '0');
+}
+_putchar('\n');
+}
