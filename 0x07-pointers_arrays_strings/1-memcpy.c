43e74230159a68eb13eab5fd147e00297b95604d
ibnbakare <ridmartrite@gmail.com>
2022-07-18 17:15:27 +0100

    adding copy memory function

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,12 +9,12 @@
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int a;
+unsigned int a;
 
-	for (a = 0; a < n; a++)
-    {
-		dest[a] = src[a];
-    }
-        dest[a++] = '\0';
-	return (dest);
+for (a = 0; a < n; a++)
+{
+dest[a] = src[a];
+}
+dest[a++] = '\0';
+return (dest);
 }
