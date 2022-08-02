2e96a90829ac256927f9434597dc380bd633be74
ridhwan <ridmartrite@gmail.com>
2022-08-02 22:33:51 +0100

    creating a macro for abs number

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,10 @@
 #ifndef FUNCTION_LIKE_MACRO
 #define FUNCTION_LIKE_MACRO
-#define ABS(x) ((x < 0) ? (x * -1):(x));
+#define ABS(x) if(x<0) \
+                return (x * -1); \
+                else \
+                return (x);
+
+
 
 #endif
