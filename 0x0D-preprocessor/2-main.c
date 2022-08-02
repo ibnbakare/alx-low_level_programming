93c221b401cd13fe90108afeae0e38cb8a329201
ridhwan <ridmartrite@gmail.com>
2022-08-02 21:57:17 +0100

    printing program file name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - prints the name of the file the program 
+ * main - prints the name of the file the program
  * was compiled from with a new line
  * Return: 0.
  */
-
-void main(void){
-    printf("%s\n",__FILE__);
-    return (0);
+void main(void)
+{
+	printf("%s\n",__FILE__);
+    	return (0);
 }
