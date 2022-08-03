a44507d2e0e00f43341596c8599f8a180c401ca5
ridhwan <ridmartrite@gmail.com>
2022-08-03 14:07:06 +0100

    adding files

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,9 +1,17 @@
 #include "function_pointers.h"
+/**
+*array_iteration -> returning the loop of an arry with a 
+*callback pointer funtion
+*@array: defining parameter array 
+*@size:defining parameter array lenght
+*@action: pointer funtion
+*Return: void
+*/
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-        for (int i = 0; i < size; i++)
-        {
-                action(array[i]);
-        }
+for (int i = 0; i < size; i++)
+{
+action(array[i]);
+}
         
 }
