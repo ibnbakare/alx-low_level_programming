4ccd91f40d4099dc2c9f8953d347779aa365e7c5
ridhwan <ridmartrite@gmail.com>
2022-08-03 13:20:29 +0100

    adding files

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,10 +2,10 @@
 /**
  * main ->print the name passed to the callback
  * funtion
- * 
  * @name:string prams
  * @f: poinyer of a fintion acting as a calback
  * function
+ * @print_name: name of function
  */
 
 void print_name(char *name, void (*f)(char *))
