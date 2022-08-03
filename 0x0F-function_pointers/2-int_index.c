377d5c4c8a3e8a1a6ff18bbb67d1e3d80d10b0c9
ridhwan <ridmartrite@gmail.com>
2022-08-03 19:25:52 +0100

    adding task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -3,16 +3,17 @@
 *@array:params
 *@size: params
 *@cmp:function
+*Return: index value or -1
 */
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
 int i;
- for (i =0; i < size; i++)
+for ( i = 0; i < size; i++ )
 {
 if(cmp(array[i]) )
 return (i);
 
-}     
-return (-1);       
+}
+return (-1); 
 }
