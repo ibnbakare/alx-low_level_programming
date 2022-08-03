317c5de78265a0897db2cedf84c4f94331fdcea3
ridhwan <ridmartrite@gmail.com>
2022-08-03 13:25:42 +0100

    adding files

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,13 @@
 #include "function_pointers.h"
 /**
- * main ->print the name passed to the callback
- * funtion
+ * print_name-> name of function
  * @name:string prams
  * @f: poinyer of a fintion acting as a calback
  * function
- * print_name-> name of function
+ * Return:void
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-        f(name);
+f(name);
 }
