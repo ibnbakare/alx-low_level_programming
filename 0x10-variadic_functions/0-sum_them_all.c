eb4e0e3c60b459a4b7e777e467eb12df9f9df5c1
ridhwan <ridmartrite@gmail.com>
2022-08-04 15:02:59 +0100

    adding vardic function for the sum of arguments

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,5 +1,5 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
+
 
 /**
  * sum_them_all - Returns the sum of all its paramters.
@@ -14,6 +14,11 @@ int sum_them_all(const unsigned int n, ...)
 	va_list nums;
 	unsigned int index, sum = 0;
 
+	if (n == 0)
+
+	{
+		return (0);
+	}
 	va_start(nums, n);
 
 	for (index = 0; index < n; index++)
