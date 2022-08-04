a0f53a5a931b793dd9ba329f315c51a9ee558590
ridhwan <ridmartrite@gmail.com>
2022-08-04 14:27:10 +0100

    adding vardic function for the sum of arguments

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,12 +3,13 @@
 *sum_them_all - returns the sum of all its parameters.
 *@n:number of arugument
 *
-*return int
+*return-int
 */
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list args;
 	if (n == 0)
+
 
 	{
 		return (0);
@@ -20,7 +21,7 @@ int sum_them_all(const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-	
+
 		int x = va_arg(args, int);
 		sum = sum + x;
 
